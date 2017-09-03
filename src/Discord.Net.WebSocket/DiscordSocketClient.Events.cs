76b734ec9c9439361c9aa970446bcf84c6230e19
Radka Janek <radka.janek@redhat.com>
2017-09-03 17:35:27 +0200

    'Added connection event `Connecting`'

--- src/Discord.Net.WebSocket/DiscordSocketClient.Events.cs
+++ src/Discord.Net.WebSocket/DiscordSocketClient.Events.cs
@@ -6,6 +6,12 @@ namespace Discord.WebSocket
     public partial class DiscordSocketClient
     {
         //General
+        public event Func<Task> Connecting
+        {
+            add { _connectingEvent.Add(value); }
+            remove { _connectingEvent.Remove(value); }
+        }
+        private readonly AsyncEvent<Func<Task>> _connectingEvent = new AsyncEvent<Func<Task>>();
         /// <summary> Fired when connected to the Discord gateway. </summary>
         public event Func<Task> Connected
         {
