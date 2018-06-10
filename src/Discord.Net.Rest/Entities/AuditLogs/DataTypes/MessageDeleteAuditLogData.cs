839b8002e6407f4ffd7b9eacfc83e38c40efc167
Radka Janek <radka.janek@redhat.com>
2018-06-10 19:38:30 +0200

    *MessageDeleteAuditLogData: Added MessageId

--- src/Discord.Net.Rest/Entities/AuditLogs/DataTypes/MessageDeleteAuditLogData.cs
+++ src/Discord.Net.Rest/Entities/AuditLogs/DataTypes/MessageDeleteAuditLogData.cs
@@ -1,22 +1,27 @@
-﻿using Model = Discord.API.AuditLog;
+﻿using System.Collections.Generic;
+using System.Linq;
+using Discord.API;
+using Model = Discord.API.AuditLog;
 using EntryModel = Discord.API.AuditLogEntry;
 
 namespace Discord.Rest
 {
     public class MessageDeleteAuditLogData : IAuditLogData
     {
-        private MessageDeleteAuditLogData(ulong channelId, int count)
+        private MessageDeleteAuditLogData(ulong channelId, int count, AuditLogChange[] changes)
         {
+            MessageIds = changes.Where(c => c.ChangedProperty == "id").SelectMany(c => c.OldValue.Values<ulong>());
             ChannelId = channelId;
             MessageCount = count;
         }
 
         internal static MessageDeleteAuditLogData Create(BaseDiscordClient discord, Model log, EntryModel entry)
         {
-            return new MessageDeleteAuditLogData(entry.Options.MessageDeleteChannelId.Value, entry.Options.MessageDeleteCount.Value);
+            return new MessageDeleteAuditLogData(entry.Options.MessageDeleteChannelId.Value, entry.Options.MessageDeleteCount.Value, entry.Changes);
         }
 
         public int MessageCount { get; }
         public ulong ChannelId { get; }
+        public IEnumerable<ulong> MessageIds { get; }
     }
 }
